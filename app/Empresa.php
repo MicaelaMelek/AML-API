2279ac85f6db5ee0891b5ec8aaed2b01b7a11211
MicaelaMelek <micaelamelek@gmail.com>
2018-06-25 23:52:23 -0300

    se agrega atributos al modelo de Empresa

--- app/Empresa.php
+++ app/Empresa.php
@@ -4,7 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Empresa
+ * @package App
+ */
 class Empresa extends Model
 {
-    //
+    protected $fillable = ['nombre', 'razon_social', 'cuit'];
 }
