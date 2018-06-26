710d7683c8ed82cd928afff04c505fab202f873a
MicaelaMelek <micaelamelek@gmail.com>
2018-06-26 11:56:04 -0300

    relacion factura con cliente y empresa

--- app/Factura.php
+++ app/Factura.php
@@ -11,4 +11,20 @@ use Illuminate\Database\Eloquent\Model;
 class Factura extends Model
 {
     protected $fillable = ['numero', 'subtotal', 'iva', 'total', 'empresa_id', 'cliente_id'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function empresa()
+    {
+        return $this->belongsTo('App\Empresa', 'empresa_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function cliente()
+    {
+        return $this->belongsTo('App\Cliente', 'cliente_id');
+    }
 }
