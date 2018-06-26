5cc08edfb4b25303987ffc9b62258733ca68f4b4
MicaelaMelek <micaelamelek@gmail.com>
2018-06-25 23:52:11 -0300

    se agrega atributos al modelo de Cliente

--- app/Cliente.php
+++ app/Cliente.php
@@ -4,7 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Cliente
+ * @package App
+ */
+
 class Cliente extends Model
 {
-    //
+    protected $fillable = ['nombre', 'cuit', 'cuil', 'empresa_id'];
 }
