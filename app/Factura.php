ec721ab2c9141e9bcbe7576a30290c0da3785ef1
MicaelaMelek <micaelamelek@gmail.com>
2018-06-25 23:52:33 -0300

    se agrega atributos al modelo de Factura

--- app/Factura.php
+++ app/Factura.php
@@ -4,7 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Factura
+ * @package App
+ */
 class Factura extends Model
 {
-    //
+    protected $fillable = ['numero', 'subtotal', 'iva', 'total', 'empresa_id', 'cliente_id'];
 }
