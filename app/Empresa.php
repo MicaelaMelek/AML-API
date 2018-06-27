a87362ec3c94bb14b0701392dfbc531f22daf5da
MicaelaMelek <micaelamelek@gmail.com>
2018-06-27 11:19:47 -0300

    se agregan los scopes para where nomdbre, cuil, cuit, empresa de empresa

--- app/Empresa.php
+++ app/Empresa.php
@@ -27,4 +27,42 @@ class Empresa extends Model
     {
         return $this->hasMany('App\Factura');
     }
+
+    /**
+     * @param $query
+     * @param $nombre
+     * @return bool
+     */
+    public function scopeWhereNombre($query, $nombre)
+    {
+        if (empty($nombre)) {
+            return false;
+        }
+
+        return $query->where('nombre', 'LIKE', $nombre);
+    }
+
+    public function scopeWhereRazonSocial($query, $razonSocial)
+    {
+        if (empty($razonSocial)) {
+            return false;
+        }
+
+        return $query->where('nombre', 'LIKE', $razonSocial);
+    }
+
+    /**
+     * @param $query
+     * @param $cuit
+     * @return bool
+     */
+    public function scopeWhereCuit($query, $cuit)
+    {
+        if (empty($cuit)) {
+            return false;
+        }
+
+        return $query->where('cuit', str_replace('-', '', $cuit));
+    }
+
 }
