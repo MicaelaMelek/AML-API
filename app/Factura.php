a448beb67976459ecadb7d8cf399eb5f40a0e272
MicaelaMelek <micaelamelek@gmail.com>
2018-06-27 11:20:36 -0300

    se agregan los scopes para where de totales, iva, cliente, empresa de factura

--- app/Factura.php
+++ app/Factura.php
@@ -27,4 +27,88 @@ class Factura extends Model
     {
         return $this->belongsTo('App\Cliente', 'cliente_id');
     }
+
+    /**
+     * @param $query
+     * @param $numero
+     * @return bool
+     */
+    public function scopeWhereNumero($query, $numero)
+    {
+        if (empty($numero)) {
+            return false;
+        }
+
+        return $query->where('numero', $numero);
+    }
+
+    /**
+     * @param $query
+     * @param $subTotal
+     * @return bool
+     */
+    public function scopeWhereSubtotal($query, $subTotal)
+    {
+        if (empty($subTotal)) {
+            return false;
+        }
+
+        return $query->where('subtotal', $subTotal);
+    }
+
+    /**
+     * @param $query
+     * @param $iva
+     * @return bool
+     */
+    public function scopeWhereIVA($query, $iva)
+    {
+        if (empty($iva)) {
+            return false;
+        }
+
+        return $query->where('iva', $iva);
+    }
+
+    /**
+     * @param $query
+     * @param $total
+     * @return bool
+     */
+    public function scopeWhereTotal($query, $total)
+    {
+        if (empty($total)) {
+            return false;
+        }
+
+        return $query->where('total', $total);
+    }
+
+    /**
+     * @param $query
+     * @param $empresa
+     * @return bool
+     */
+    public function scopeWhereEmpresa($query, $empresa)
+    {
+        if (!empty($empresa)) {
+            return $query->whereHas('empresa', function ($query) use ($empresa) {
+                return $query->where('nombre', $empresa);
+            });
+        }
+
+        return false;
+    }
+
+    public function scopeWhereCliente($query, $cliente)
+    {
+        if (!empty($cliente)) {
+            return $query->whereHas('cliente', function ($query) use ($cliente) {
+                return $query->where('nombre', $cliente);
+            });
+        }
+
+        return false;
+    }
+
 }
