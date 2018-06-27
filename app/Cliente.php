63abc513cf3a478edfc950b43e04f2db706d61b1
MicaelaMelek <micaelamelek@gmail.com>
2018-06-27 11:19:09 -0300

    se agregan los scopes para where nombre, cuil, cuit, empresa de cliente

--- app/Cliente.php
+++ app/Cliente.php
@@ -28,4 +28,62 @@ class Cliente extends Model
     {
         return $this->hasMany('App\Factura');
     }
+
+    /**
+     * @param $query
+     * @param $nombre
+     * @return bool
+     */
+    public function scopeWhereNombre($query, $nombre)
+    {
+        if (empty($nombre)) {
+            return false;
+        }
+
+        return $query->where('nombre', 'LIKE', $nombre);
+    }
+
+    /**
+     * @param $query
+     * @param $cuil
+     * @return bool
+     */
+    public function scopeWhereCuil($query, $cuil)
+    {
+        if (empty($cuil)) {
+            return false;
+        }
+
+        return $query->where('cuil', str_replace('-', '', $cuil));
+    }
+
+    /**
+     * @param $query
+     * @param $cuit
+     * @return bool
+     */
+    public function scopeWhereCuit($query, $cuit)
+    {
+        if (empty($cuit)) {
+            return false;
+        }
+
+        return $query->where('cuit', str_replace('-', '', $cuit));
+    }
+
+    /**
+     * @param $query
+     * @param $empresa
+     * @return bool
+     */
+    public function scopeWhereEmpresa($query, $empresa)
+    {
+        if (!empty($empresa)) {
+            return $query->whereHas('empresa', function ($query) use ($empresa) {
+                return $query->where('nombre', $empresa);
+            });
+        }
+
+        return false;
+    }
 }
